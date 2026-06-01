41776e81c3d5d5247e7646b90c29acf5eb6ddd8e
sansui233 <sansuilnm@gmail.com>
2026-06-02 04:34:24 +0800

    update title width on mobile

--- app/routes/posts.$slug.tsx
+++ app/routes/posts.$slug.tsx
@@ -97,7 +97,9 @@ export default function BlogPost({ loaderData }: Route.ComponentProps) {
       {/* Main article */}
       <div className="mx-auto w-170 px-5 py-15 max-xl:w-[calc(100%-480px)] max-xl:max-w-170 max-lg:w-auto max-lg:max-w-170 max-sm:w-full max-sm:py-12">
         <article className="relative">
-          <h1 className="mx-auto mt-0 mb-0 w-4/5 text-center">{post.title}</h1>
+          <h1 className="mx-auto mt-0 mb-0 text-center sm:w-4/5">
+            {post.title}
+          </h1>
           <PostMeta
             date={post.date}
             categories={post.categories}
