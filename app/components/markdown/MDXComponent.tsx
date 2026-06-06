a5690accd3fc64d1eba95b152a2a669179e223ac
sansui233 <sansuilnm@gmail.com>
2026-06-07 01:12:55 +0800

    prevent re-render

--- app/components/markdown/MDXComponent.tsx
+++ app/components/markdown/MDXComponent.tsx
@@ -1,13 +1,22 @@
 import { runSync } from "@mdx-js/mdx";
+import { useMemo, type ComponentType } from "react";
 import * as runtime from "react/jsx-runtime";
 
 interface MDXProps {
   code: string;
-  components?: Record<string, React.ComponentType<any>>;
+  components?: Record<string, ComponentType<any>>;
 }
+
+type MDXModule = {
+  default: ComponentType<{
+    components?: Record<string, ComponentType<any>>;
+  }>;
+};
 
 // parse the Velite generated MDX code into a React component function
 export const MDXContent = ({ code, components }: MDXProps) => {
-  const MDXModule = runSync(code, runtime);
-  return <MDXModule.default components={{ ...components }} />;
+  const MDXModule = useMemo(() => runSync(code, runtime) as MDXModule, [code]);
+  const mdxComponents = useMemo(() => ({ ...components }), [components]);
+
+  return <MDXModule.default components={mdxComponents} />;
 };
